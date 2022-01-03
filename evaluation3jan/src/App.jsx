3ab710bd45e064f609418d50e5a2e6cd1e48cd87
deepesh06-95 <91532309+deepesh06-95@users.noreply.github.com>
2022-01-03 15:02:08 +0530

    eval

---
+++ evaluation3jan/src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { Form } from './components/Form';
+function App() {
+  return (
+    <div className="App">
+      hello
+      <div className='griding'>
+      <div className='left_form'> 
+      <h3>Enter reciepe details</h3>
+      <Form className="lef"/></div>
+      <div></div>
+      </div>
+      
+     
+    </div>
+  );
+}
+
+export default App;
